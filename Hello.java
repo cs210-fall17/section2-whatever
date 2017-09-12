eacc9e7a4c5efb21ceb1f4982f7ba3dce96671da
Michael Dunlap <Michael@dhcp-10-134-215-206.uawifi.arizona.edu>
2017-09-11 19:39:08 -0700

    Gold Leader finished.

--- Hello.java
+++ Hello.java
@@ -13,12 +13,13 @@ public class Hello {
         // Red Leader should add a print statement that says "Lock S-foils in attack position."
         System.out.print("Lock S-foils in attack position.");
         // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
-
+        System.out.println("Stabilize your rear deflectors... Watch for enemy fighters.");
     }
 
     public static void goldLeader() {
 
         // Gold Leader should add a print statement that says "Loosen up!"
+        System.out.println("Losen up!");
 
 
     }
